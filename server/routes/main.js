c551bab18c58722d53c57757ff1f4e4349410120
jrikula <jrikula@gmail.com>
2024-01-12 20:03:37 +0200

    Part 5
    Creating article data and getting articles from the DB

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
+// GET /HOME
 
-// Routes
-router.get('', (req, res) => {
-    const locals = {
-        title: "NodeJs Blog",
-        description: "Simple Blog created with NodeJs, Express & MongoDb."
+router.get('', async (req, res) => {
+    try {
+        const locals = {
+            title: "NodeJs Blog",
+            description: "Simple Blog created with NodeJs, Express & MongoDb."
+        }
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+
+    } catch (error) {
+        console.log(error);
     }
-
-    res.render('index', { locals });
 });
 
 router.get('/about', (req, res) => {
